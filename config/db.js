eb20fd10b7ec6b34544b0bf1cfbf633714d5eda3
abhishekkumar <abhishekkumar0841@gmail.com>
2023-09-12 00:12:31 +0530

    CURD in backend

---
+++ config/db.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose')
+
+const connectToDb = async()=>{
+    mongoose.connect(process.env.MONGODB_URI)
+    .then((conn)=>{
+        console.log(`Connected to DB: ${conn.connection.host}`)
+    }).catch((err)=>{
+        console.log(err.message)
+        process.exit(1);
+    })
+}
+
+module.exports = connectToDb;
